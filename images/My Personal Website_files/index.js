26e9a29bf88f0500e196ab5855fe944f9eeb9eaf
DANNJUGUNA <ndan30071@gmail.com>
2022-11-20 23:58:48 +0300

    finished html and css

---
+++ images/My Personal Website_files/index.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Iframe is loaded.";
+  }
+  function myJs(){
+    var popup = document.getElementById("js");
+    popup.classList.toggle("show");
+  }
