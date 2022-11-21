f8b464c0c6fc1f64714f5928174aae8ba35a78fd
DANNJUGUNA <ndan30071@gmail.com>
2022-11-21 07:53:52 +0300

    added event

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-function myFunction() {
-    document.getElementById("demo").innerHTML = "Iframe is loaded.";
-  }
-  function myJs(){
-    var popup = document.getElementById("js");
-    popup.classList.toggle("show");
-  }
+document.getElementById("image1").addEventListener("mouseover", mouseOver);
+document.getElementById("image1").addEventListener("mouseout", mouseOut);
+
+function mouseOver1() {
+  document.getElementById("image1").style.background= "black";
+  
+  
+}
+
+function mouseOut1() {
+  document.getElementById("image1").style.background= "white";
+}
+document.getElementById("image2").addEventListener("mouseover", mouseOver);
+document.getElementById("image2").addEventListener("mouseout", mouseOut);
+
+function mouseOver() {
+  document.getElementById("image2").style.background= "red";
+  
+  
+}
+
+function mouseOut() {
+  document.getElementById("image2").style.background= "white";
+}
